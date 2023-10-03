0ee365c13e413781799df908d0aad9f5fa75dc98
seanarch <zhuqingyun@outlook.com>
2023-10-03 17:28:10 -0600

    class-based api

--- BookListAPI/urls.py
+++ BookListAPI/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path 
-from .import views 
+from . import views 
 
 urlpatterns = [
-    path('books/', views.books)
+    # path('books/', views.books) 
+    # use class based view
+    path('books/', views.BookList.as_view()),
 ]
--- BookListAPI/views.py
+++ BookListAPI/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 from rest_framework.response import Response 
 from rest_framework import status 
-from rest_framework.decorators import api_view 
+from rest_framework.decorators import api_view
+from rest_framework.views import APIView
+from django.http import HttpResponse
 
-# Create your views here.
-@api_view() 
+# # Create your views here.
+# API view decorator in DRF 
+@api_view(['GET','POST']) 
 def books(request): 
+    # return HttpResponse('list of the books', status=status.HTTP_200_OK)
     return Response('list of the books', status=status.HTTP_200_OK)
+
+class BookList(APIView): 
+    def get(self, request): 
+        return Response({"message": "list of the books"}, status.HTTP_200_OK)
+    
+    def post(self, request): 
+        return Response({"message": "new book created"}, status.HTTP_201_CREATED)
